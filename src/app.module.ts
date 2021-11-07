bda15e7c6c7712efc303f03fdb2313219e801af8
Hyagobsantos <hyagobezerra64@gmail.com>
2021-11-07 19:04:25 -0300

    alterando estrutura total

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { PrismaModule } from './prisma/prisma.module';
-import { AuthModule } from './auth/auth.module';
-import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule, AuthModule, ProductsModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })

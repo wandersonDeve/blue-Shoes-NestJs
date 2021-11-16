6a3bcc119dbe42414d790d2025f695f924864342
Felipe Menezes <sa.felipem@gmail.com>
2021-11-16 20:10:22 -0300

    change database

--- prisma/migrations/20211115020548_adicicao_preco/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `preco` to the `Produto` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `produto` ADD COLUMN `preco` DOUBLE NOT NULL;

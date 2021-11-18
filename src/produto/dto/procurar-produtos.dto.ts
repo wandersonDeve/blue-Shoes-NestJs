4e9e04d167be1576b50047ad56e15bf02412a507
Felipe Menezes <sa.felipem@gmail.com>
2021-11-17 23:51:55 -0300

    BSP-95 cleaning code

--- src/produto/dto/procurar-produtos.dto.ts
+++ src/produto/dto/procurar-produtos.dto.ts
@@ -7,21 +7,3 @@ export class ProcurarProdutosQueryDto {
   @IsOptional()
   marca: string;
 }
-
-// async findAll(query: ProcurarProdutosQueryDto): Promise<any> {
-//     const res = await this.db.produto.findMany({
-//       where: {
-//         nome: {
-//           contains: query.nome,
-//           mode: 'insensitive',
-//         },
-//       },
-//     });
-
-//     return res;
-//   }
-// @Get('/todos')
-//   @UsePipes(ValidationPipe)
-//   async findMany(@Query() query: AtualizarProdutoDto): Promise<any> {
-//     return this.produto.findAll(query);
-//   }
